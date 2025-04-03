e04dada1425f2455667a9a0ab481831603415dc5
fairtale5 <syberjj@gmail.com>
2025-04-03 01:12:36 -0300

    Update fix_permissions.sh to include target directory

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -4,6 +4,6 @@
 echo "Fixing ownership and permissions..."
 sudo chown -R $(whoami):$(whoami) .
 chmod -R 755 .
-chmod -R 777 build .svelte-kit node_modules
+chmod -R 777 build .svelte-kit node_modules target
 
 echo "Done!"
